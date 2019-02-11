1e4e17abad3025b705b28b5fcf535ea4abf908c4
Giancarlo Fusiello <g.fusiello@elifesciences.org>
2019-02-11 12:30:18 +0000

    add setup.py to libero schemas

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='libero schemas',
+    version='0.1',
+    description='libero xml api schemas',
+    url='http://github.com/libero/schemas',
+    author='eLife sciences publications, ltd.',
+    license='MIT',
+    zip_safe=False
+)
