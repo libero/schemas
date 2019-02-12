9665169adfcef34e686450424654f6eb69f5a733
Chris Wilkinson <chriswilkinson84@gmail.com>
2019-02-12 14:55:39 +0000

    Update setup.py
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='libero schemas',
     version='0.1',
     description='libero xml api schemas',
-    url='http://github.com/libero/schemas',
+    url='https://github.com/libero/schemas',
     author='eLife sciences publications, ltd.',
     license='MIT',
     zip_safe=False
