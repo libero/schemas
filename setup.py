db85739a6a98e630a6fdcfbb187242417beaf9fa
Chris Wilkinson <chriswilkinson84@gmail.com>
2019-02-12 14:56:52 +0000

    Update setup.py
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='libero schemas',
     version='0.1',
-    description='libero xml api schemas',
+    description='Libero data model schemas',
     url='https://github.com/libero/schemas',
     maintainer='eLife Sciences Publications, Ltd',
     license='MIT',
