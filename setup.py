cebfb5fb66b3bd7b820e2cebf50f9cc471d5204c
Giancarlo Fusiello <carlofusiello@gmail.com>
2019-02-12 15:29:22 +0000

    add setup.py to libero schemas (#16)
    * add setup.py to libero schemas
    
    * add packages to setup.py
    
    * remove packages from setup
    
    * Update setup.py
    
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>
    
    * Update setup.py
    
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>
    
    * Update setup.py
    
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>
    
    * Update setup.py
    
    Co-Authored-By: GiancarloFusiello <carlofusiello@gmail.com>
    
    * change package name

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='libero-schemas',
+    version='0.0.1',
+    description='Libero data model schemas',
+    url='https://github.com/libero/schemas',
+    maintainer='eLife Sciences Publications, Ltd',
+    license='MIT',
+    zip_safe=False
+)
